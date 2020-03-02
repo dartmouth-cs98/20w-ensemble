062bca9e706f3a7e5e2ccabe00324535c92abee6
Bryan Shin <bryanshin1997@gmail.com>
2020-03-02 00:22:07 -0500

    fixed

---
+++ Assets/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void teleport() {
+    	camera.transform.position = camera.transform.position - this.transform.position;
+    }
+}
