0eb1010db541d4c6599b6c6165da15eb5b7f2d7f
soohwan0905 <soohwan0905@gmail.com>
2020-03-05 06:13:03 -0500

    refined frontstage. backstage transition added

---
+++ Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+
+public struct Gesture{
+
+    public string name;
+    public List<Vector3> fingerDatas;
+    public UnityEvent onRecognized;
+}
+
+
+
+public class GestureDetector : MonoBehaviour
+{
+    public OVRSkeleton skeleton;
+    private List<OVRBone> fingerBones;
+    public List<Gesture> gestures;
+    public bool debugMode = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fingerBones=new List<OVRBone>(skeleton.Bones);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(debugMode && Input.GetKeyDown(KeyCode.Space)){
+            Save();
+        }
+    }
+
+    void Save(){
+        Gesture g = new Gesture();
+        g.name = "New Gesture";
+        List<Vector3> data = new List<Vector3>();
+        foreach (var bone in fingerBones)
+        {
+            //finger position relative to root
+            data.Add(skeleton.transform.InverseTransformPoint(bone.Transform.position));
+        }
+        g.fingerDatas=data;
+        gestures.Add(g);
+    }
+}
