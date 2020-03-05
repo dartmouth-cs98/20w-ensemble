2a5f5635624b4f8e149edd5b47329b1dc9193c01
soohwan0905 <soohwan0905@gmail.com>
2020-03-05 16:07:00 -0500

    Mainstage functionality refined

--- Assets/Scripts/Muter.cs
+++ Assets/Scripts/Muter.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+
+public struct MuteManager{
+    public bool isMute;
+
+}
+
 public class Muter : MonoBehaviour
 {
+    MuteManager muteManager;
     // Update is called once per frame
     public void muteUnmute(AudioSource audio)
     {
         audio.mute=!audio.mute;
+        muteManager.isMute=audio.mute;
+    }
+    
+    public void muteColor(GameObject sphere)
+    {
+        var sphereRenderer = sphere.GetComponent<Renderer>();
+
+        if (muteManager.isMute){
+            sphereRenderer.material.SetColor("_Color", Color.red);
+        }
+        else{
+            sphereRenderer.material.SetColor("_Color", Color.green);
+        }
     }
 }
