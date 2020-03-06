c82f68c51bd18b9298c5cbc793bcab8de11f2237
soohwan0905 <soohwan0905@gmail.com>
2020-03-05 22:10:01 -0500

    audio configuration

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip otherClip;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying){
+            audioSource.clip=otherClip;
+            audioSource.Play();
+        }
+    }
+
+    // AudioSource myAudio;
+    // void Start(){
+    //     myAudio = GetComponent<AudioSource>();
+    //     Invoke("audioFinished",myAudio.clip.length);
+    // }
+
+    // void audioFinished(){
+    //     Debug.Log("Audio Finished");
+    // }
+}
---
+++ Assets/Scripts/TextTimer.cs
@@ -0,0 +1,14 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class TextTimer : MonoBehaviour
+ {
+     
+     public float time = 20; //Seconds to read the text
+ 
+     IEnumerator Start ()
+     {
+         yield return new WaitForSeconds(time);
+         Destroy(gameObject);
+     }
+ }
