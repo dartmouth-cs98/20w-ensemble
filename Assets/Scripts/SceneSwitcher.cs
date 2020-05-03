2baab14ab063c62b788da3a65cb89904ed48024d
soohwan0905 <soohwan0905@gmail.com>
2020-05-03 23:59:00 +0900

    Backstage: onlybook functionality left

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,21 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void moveForward(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    public void moveForward()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void moveBack(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+    public void moveBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
-    public void moveBackStage(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-2);
+    public void moveBackTwo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 
-    public void goToFrontStage() {
-    	SceneManager.LoadScene(3);
+    public void moveForwardTwo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
-    public void restartTutorial(){
+    public void goToFrontStage()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void restartTutorial()
+    {
         SceneManager.LoadScene(0);
     }
 }
