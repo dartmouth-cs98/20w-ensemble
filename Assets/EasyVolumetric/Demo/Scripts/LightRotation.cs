1cbb36c44b677078fbc9bfb947d8ac6ed6e44207
MarshallPeng <marshall.s.peng.20@dartmouth.edu>
2020-06-08 00:58:37 -0500

    Volumetric lights

---
+++ Assets/EasyVolumetric/Demo/Scripts/LightRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 rotation;
+
+    private void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime, Space.World);
+    }
+
+}
